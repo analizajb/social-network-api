c601e7174a08fc15487579a80320bd1836138f3f
analizajb <96861913+analizajb@users.noreply.github.com>
2022-09-10 10:24:44 -0700

    installing moment.js and adding format to Thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,5 @@
 const { Schema, model, Types } = require("mongoose");
+const moment = require("moment");
 
 const ReactionSchema = new Schema(
   {
@@ -19,7 +20,7 @@ const ReactionSchema = new Schema(
     createdAt: {
       type: Date,
       default: Date.now,
-      get: (createdAtVal) => dateFormat(createdAtVal),
+      get: (createdAtVal) => moment(createdAtVal).format("MMM DD YYYY hh:mm a"),
     },
   },
   {
@@ -41,7 +42,7 @@ const ThoughtSchema = new Schema(
     createdAt: {
       type: Date,
       default: Date.now,
-      get: (createdAtVal) => dateFormat(createdAtVal),
+      get: (createdAtVal) => moment(createdAtVal).format("MMM DD YYYY hh:mm a"),
     },
     username: {
       type: String,
